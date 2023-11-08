53ead3c21a6a27f2e95be9f22c44c6150e1e46b3
+ <lmcjcr@gmail.com>
2023-11-08 10:02:40 -0500

    exer7

---
+++ exer7_hash.rb
@@ -0,0 +1,16 @@
+# difference between the two hashes that were created?
+x = "hi there"
+my_hash = {x: "some value"}  # used a symbol x as key
+my_hash2 = {x => "some value"}  #used the string value x variable as the key.
+
+
+
+# upto
+arr = []
+5.upto(10) { |i| arr << i}
+puts arr
+
+# times
+array = []
+5.times {|i| array.push(i) }
+puts array
