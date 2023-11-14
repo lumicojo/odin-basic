84bfbe2f63577e2a74616f7bb9125798385cb772
+ <lmcjcr@gmail.com>
2023-11-14 09:40:25 -0500

    prepend-append

--- exe3.rb
+++ exe3.rb
@@ -3,3 +3,12 @@
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 puts arr.select { |num| num.odd?}
+
+
+# one line version
+new_array = arr.select { |number| number % 2 != 0 }
+
+# multi-line version
+new_array = arr.select do |number|
+  number % 2 != 0
+end
---
+++ exe4.rb
@@ -0,0 +1,4 @@
+#Append 11 to the end of the original array. Prepend 0 to the beginning.
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+puts arr.prepend 0  #push or <<
+puts arr.append 11  # unshift
