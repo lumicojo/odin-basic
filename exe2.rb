630b534b62f86889bdc7301b036910221a348b7c
+ <lmcjcr@gmail.com>
2023-11-14 09:06:38 -0500

    find value >5

--- exe2.rb
+++ exe2.rb
@@ -1,3 +1,4 @@
 # Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# tprint out values greater than 5
+# to print out values greater than 5
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+puts arr.select {|value| value > 5}
