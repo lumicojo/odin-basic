5c84b1b613990b68eaca8d60461032922e37adb3
+ <lmcjcr@gmail.com>
2023-11-16 15:17:34 -0500

    use map and split to take the string apart

---
+++ exe14.rb
@@ -0,0 +1,10 @@
+# Turn  this array into a new array that consists of strings containing one word.
+# (ex. ["white snow", etc...] → ["white", "snow", etc...].
+#Look into using Array's map and flatten methods, as well as String's split method.
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+      'slippery sidewalk', 'salted roads', 'white trees']
+
+a = a.map { |words| words.split}
+a = a.flatten
+p a
