415da342cd251f1a3560e39f5cdda408d8b3253c
+ <lmcjcr@gmail.com>
2023-11-20 13:40:12 -0500

    enumerable using the old way for loop  select and reject method basic

---
+++ basic_enumerable.rb
@@ -0,0 +1,22 @@
+#before enumerable  (old way)
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+invited_list = []
+
+for friend in friends do
+  if friend != 'Brian'
+  invited_list.push(friend)
+  end
+end
+puts invited_list
+
+
+
+# New way using select enumerable
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+p friends.select {|friend| friend != 'Brian'}
+
+##################   or use reject ven better
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+p friends.reject {|friend| friend == 'Brian'}
